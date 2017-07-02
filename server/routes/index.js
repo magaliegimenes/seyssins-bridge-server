1a2c1338db0d8e424e2d01bae52c3a64e2adc2cd
nMagnat <magnat.nicolas@gmail.com>
2017-07-02 22:59:18 +0200

    Adding competition model and indexes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Actualities = require('../services/actualities');
+const Competitions = require('../services/competitions');
 const Users = require('../services/users');
 
 module.exports.init = (app) => {
@@ -8,6 +9,10 @@ module.exports.init = (app) => {
   app.post('/api/actualities', Users.isAuthenticated, Actualities.post);
   app.put('/api/actualities/:id', Users.isAuthenticated, Actualities.put);
   app.delete('/api/actualities/:id', Users.isAuthenticated, Actualities.delete);
+  app.get('/api/competitions', Competitions.get);
+  app.post('/api/competitions', Users.isAuthenticated, Competitions.post);
+  app.put('/api/competitions/:id', Users.isAuthenticated, Competitions.put);
+  app.delete('/api/competitions/:id', Users.isAuthenticated, Competitions.delete);
   app.get('/api/auth', Users.isAuthenticated, (req, res) => res.status(200).send({status: 'authenticated'}));
   app.post('/api/auth', Users.authenticate);
   app.post('/set-user', Users.isAuthenticated, Users.post);
---
+++ server/services/competitions/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const CompetitionModel = require('./competitions.model');
+
+module.exports.get = (req, res) => {
+  return CompetitionModel.find().sort('-createdAt')
+    .then(competitions => {
+    setTimeout(() => res.send(competitions), 10000);
+  // res.send(competitions);
+})
+.catch(err => {
+    res.status(500).send({
+    message: err.message
+  })
+});
+};
+
+module.exports.post = (req, res) => {
+  let competition = new CompetitionModel(req.body);
+  return competition.save()
+      .then(competitionSaved => {
+      res.send(competitionSaved);
+})
+.catch(err => {
+    res.status(500).send({
+    message: err.message
+  });
+});
+};
+
+module.exports.put = (req, res) => {
+  return CompetitionModel.findOneAndUpdate({_id: req.params.id}, req.body).exec()
+      .then(competition => {
+      res.send(competition);
+})
+.catch(err => {
+    res.status(500).send({
+    message: err.message
+  });
+});
+};
+
+module.exports.delete = (req, res) => {
+  let id = req.params.id;
+  return CompetitionModel.remove({_id: id})
+      .then(() => {
+      res.send({message: 'competition deleted'});
+})
+.catch(err => {
+    res.status(500).send({
+    message: err.message
+  });
+});
+};
